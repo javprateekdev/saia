9d1e59b16c06564dd83416ed6b1f48910d6dde57
Prateek <javprateekdev@gmail.com>
2023-01-13 09:13:26 +0530

    first commit

---
+++ src/compoent/Services.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Services = () => {
+  return (
+    <>
+      
+       
+        <div style={{width:"100%",border:"1px solid red",display:"flex",flexWrap:"Wrap"}}>
+        
+        <div style={{height:"300px",width:"300px",background:"blue",margin:"10px"}}></div>
+        <div style={{height:"300px",width:"300px",background:"blue",margin:"10px"}}></div>
+        <div style={{height:"300px",width:"300px",background:"blue",margin:"10px"}}></div>
+        <div style={{height:"300px",width:"300px",background:"blue",margin:"10px"}}></div>
+        <div style={{height:"300px",width:"300px",background:"blue",margin:"10px"}}></div>
+
+           
+        </div>
+      
+    </>
+  )
+}
+export default Services
---
+++ src/compoent/Skills.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Skills = () => {
+  return (
+    <>
+      <section className='hero'>
+        <h1>Welcome To  Page</h1>
+      </section>
+    </>
+  )
+}
+export default Skills
